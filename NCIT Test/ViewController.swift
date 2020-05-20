dacaef9d93c7c5402ac66a57af5b36081253669a
islamXdeceloper <islamxdeveloper@gmail.com>
2020-05-21 00:45:45 +0200

    Initial Commit

---
+++ NCIT Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NCIT Test
+//
+//  Created by Eslam Ahmed on 5/21/20.
+//  Copyright © 2020 Eslam Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
